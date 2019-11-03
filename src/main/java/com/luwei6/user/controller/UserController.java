706403520afcf3cb5449be293d35c5bf39210972
60270 <397072113@qq.com>
2019-11-04 02:22:33 +0800

    增加bootstrap-table插件

---
+++ src/main/java/com/luwei6/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.luwei6.user.controller;
+
+import com.luwei6.lottery.model.LotteryUnionLottoDto;
+import com.luwei6.lottery.services.LotteryUnionLottoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/10/26.
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping("/helloWorld")
+    public String toIndex() throws Exception {
+        return "/helloWorld";
+    }
+
+    @RequestMapping("/dualColored/genDualColorLottery")
+    public String genDualColorLottery() throws Exception {
+        return "/dualColored/genDualColorLottery";
+    }
+}
